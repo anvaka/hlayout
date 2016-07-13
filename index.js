b02dc8e79c92d74d44523af7a5a039fcce6fd66e
anvaka <anvaka@gmail.com>
2016-07-12 21:01:00 -0700

    Added initial interface

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = createLayout;
+
+function createLayout(graph) {
+  var api = {
+    getNodePosition: getNodePosition,
+    run: run
+  };
+
+  return api;
+
+  function getNodePosition(nodeId) {
+    
+  }
+
+  function run() {
+    
+  }
+}
---
+++ test/index.js
@@ -0,0 +1,19 @@
+var test = require('tap').test;
+var createLayout = require('../');
+var createMiserablesGraph = require('miserables').create;
+
+test('it can run the layout', function(t) {
+  var graph = createMiserablesGraph();
+  var layout = createLayout(graph);
+  layout.run();
+
+  graph.forEachNode(function(node) {
+    var position = layout.getNodePosition(node.id);
+
+    t.ok(typeof position.x === 'number' &&
+         typeof position.y === 'number',
+         'Position is defined for ', node.id);
+  });
+
+  t.end();
+});
